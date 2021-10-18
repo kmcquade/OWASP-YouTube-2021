3342f81e81c8ce131c31edded0d2cdd094285187
Kinnaird McQuade <kinnairdm@gmail.com>
2021-10-17 18:57:50 -0700

    first commit

---
+++ resource-exposure/README.md
@@ -0,0 +1,4 @@
+# AWS Resource Exposure modules
+
+This Terraform module contains resources that are exposed via overly permissive resource policies (i.e., cross account access).
+
---
+++ resource-exposure/variables.tf
@@ -0,0 +1,9 @@
+variable "name" {
+  type    = string
+  default = "test-resource-exposure"
+}
+
+variable "name_prefix" {
+  type    = string
+  default = "test-resource-exposure"
+}
