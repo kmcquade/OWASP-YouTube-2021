1440afc01fb5665db97c6a2602409cfacb704d73
Kinnaird McQuade <kinnairdm@gmail.com>
2021-10-24 00:39:19 -0700

    Add the prowler reports

--- scripts/demo-iam-vulnerable.sh
+++ scripts/demo-iam-vulnerable.sh
@@ -8,6 +8,7 @@ export AWS_DEFAULT_PROFILE="vulnerable-aws"
 export AWS_PROFILE="vulnerable-aws"
 export AWS_DEFAULT_REGION="us-east-1"
 pwd
+tfenv use 0.12.28
 terraform init
 terraform get
 terraform plan
--- scripts/demo-resource-exposure.sh
+++ scripts/demo-resource-exposure.sh
@@ -7,6 +7,7 @@ export AWS_DEFAULT_PROFILE="vulnerable-aws"
 export AWS_PROFILE="vulnerable-aws"
 export AWS_DEFAULT_REGION="us-east-1"
 pwd
+tfenv use 1.0.6
 terraform init
 terraform get
 terraform plan
